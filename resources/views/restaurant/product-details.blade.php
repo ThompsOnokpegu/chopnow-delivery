de075ac016dbec4f36445fdb896df22e259ae596
AJThompson <tommyriode@gmail.com>
2023-10-28 07:57:10 +0100

    Product Details Page Scalfolding

--- resources/views/restaurant/product-details.blade.php
+++ resources/views/restaurant/product-details.blade.php
@@ -1,7 +1,7 @@
 @extends('restaurant.layouts.main')
 
 @section('title')
-
+ {{ $menu->name }}
 @endsection
 @section('content')
 <div class="container">
@@ -13,28 +13,24 @@
         </div>
         <div class="product-details-wrap text-center">
             <h5>{{ $menu->name }}</h5>
-            <p>Mixed Pizza with Cheese</p>
-            <img src="{{ asset('customer/assets/img/item/single-product.png') }}" alt="img">
+            {{-- <p>Mixed Pizza with Cheese</p> --}}
+            <img src="{{ asset('product-images/'.$menu->product_image) }}" width="200" class="rounded-2 mt-3 mb-3" alt="img">
         </div>
-        <div class="wrap-details">
-            <h6>Product details</h6>
-            <p>The Original Pan pizza and the iconic Stuffed Crust pizza. A layer of toasted parmesan is baked into the crust of the Original Pan pizza.</p>
+        <div class="wrap-details text-center">
+            <h6>Product Description</h6>
+            <p>{{ $menu->description }}</p>
         </div>
-        <div class="product-size">
-            {{-- <ul class="size">
-                <li><a href="#">S</a></li>
-                <li><a href="#">M</a></li>
-                <li><a href="#">L</a></li>
-            </ul> --}}
-            <form>
+        <div class="product-siz text-center mb-5">
+            <form class="mt-5 mb-4">
                 <div class="quantity buttons_added">
                     <input type="button" value="-" class="minus">
                     <input type="number" class="input-text qty text" step="1" min="1" max="10000" name="quantity" value="1">
                     <input type="button" value="+" class="plus">
                 </div>
+                <button style="border:1px solid#CE2829; " class="btn btn-bas w-20 text-center" href="#"><i class="ri-add-line"></i> Add to Cart</button>
             </form>
         </div>
-        <div class="wrap-details">
+        <div class="wrap-details text-center">
             <h6>Special instructions</h6>
             <p>Please let us know if you are allergic to anything or if we need to avoid anything.</p>
         </div>
