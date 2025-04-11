8332e628e82e9587395f224ea31ae8956a8a0a30
AJThompson <tommyriode@gmail.com>
2025-04-11 09:51:27 +0100

    Set Default Kitchen Banner

--- app/Http/Controllers/VendorAuthController.php
+++ app/Http/Controllers/VendorAuthController.php
@@ -67,7 +67,7 @@ class VendorAuthController extends Controller
                      
         $vendor->first_name = $request->first_name;
         $vendor->email = $request->email;
-        $vendor->kitchen_banner_image="brand-image.png";
+        $vendor->kitchen_banner_image="v1744361220/chopnow/vendor-banners/v8z2sycu02qiqauqqaey.png";//default kitchen banner - cloudinary
         $vendor->password = Hash::make($request->password);
         $vendor->created_at = Carbon::now();
 
